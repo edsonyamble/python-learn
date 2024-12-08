3692798185f74a9715b92a60dd14a8390d91790a
Edson Yamble <edsonyamble2@gmail.com>
2024-12-08 12:30:45 +0700

    print learn

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Привет,Мир!")
+# print permet de montrer le resultat
+print("Привет\'2',Мир!")
+#\permet de metre des symbole dans print
+print("Привет\n'3',Мир!")
+#\npermet de tourner a lingne suivante
+print("Привет\t'4',Мир!")
+#\tpermet de metre de l espace
+#parametre print sep and end
+print('hello','world',sep='@@@')
+#sep vas ajouter soncontenu dans l espace
+print('hello','world',end='!')
+#end add ton end of script по  default and это пустая строка
+print('hello','world',sep='@@@@',end='!')
+'''
+print('hello')
+'''
+# для коментарии # либо ''' ''' это задокументирование строка интерпретатор ек читает
